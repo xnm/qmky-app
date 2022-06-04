29dcaac2d768aa2753cb1fb5d3eab43193769a84
sosohime <supersosohime@gmail.com>
2022-06-04 21:32:01 +0800

    add tab-view

--- apps/qmky-app/src/app/index.tsx
+++ apps/qmky-app/src/app/index.tsx
@@ -1,50 +1,17 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 import React, { useState } from 'react';
-import {
-  StyleSheet,
-  View,
-  // Text,
-  Button,
-} from 'react-native';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { NativeBaseProvider, Box, HStack, VStack, Pressable, Image, Text } from "native-base";
+import {
+  NativeBaseProvider,
+  View, Button, Text } from "native-base";
+
+import { Square } from '../pages/square'
 
 import BookSVG from '../assets/icons/book.svg'
 import HeartSVG from '../assets/icons/heart.svg'
 import CoursesSVG from '../assets/icons/courses.svg'
-
-function Home({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>今天还没人磕！快来成为第一个！</Text>
-      <Box bg="primary.600" py="4" px="3" borderRadius="5" rounded="md" width={375} maxWidth="100%">
-        <HStack justifyContent="space-between">
-          <Box justifyContent="space-between">
-            <VStack space="2">
-              <Text fontSize="sm" color="white">
-                Today @ 9PM
-              </Text>
-              <Text color="white" fontSize="xl">
-                Let's talk about avatar!
-              </Text>
-            </VStack>
-            <Pressable rounded="xs" bg="primary.400" alignSelf="flex-start" py="1" px="3">
-              <Text textTransform="uppercase" fontSize="sm" fontWeight="bold" color="white">
-                Remind me
-              </Text>
-            </Pressable>
-          </Box>
-          <Image source={{
-          uri: 'https://media.vanityfair.com/photos/5ba12e6d42b9d16f4545aa19/3:2/w_1998,h_1332,c_limit/t-Avatar-The-Last-Airbender-Live-Action.jpg'
-        }} alt="Aang flying and surrounded by clouds" height="100" rounded="full" width="100" />
-        </HStack>
-      </Box>
-    </View>
-  );
-}
 
 function DetailsScreen({ navigation }) {
   return (
@@ -71,16 +38,18 @@ function App() {
       <NativeBaseProvider>
         <Tab.Navigator>
           <Tab.Screen
-            name="Home"
+            name="Square"
             options={{
+              header: () => null,
               title: '磕头广场',
               tabBarIcon: ({ focused }) => <BookSVG width={24} height={24} stroke={focused ? "#0066FF" : "#000000"} />
             }}
-            component={Home}
+            component={Square}
           />
           <Tab.Screen
             name="Settings"
             options={{
+              header: () => null,
               title: '收藏',
               tabBarIcon: ({ focused }) => <HeartSVG width={24} height={24} stroke={focused ? "#0066FF" : "#000000"} />
             }}
@@ -89,6 +58,7 @@ function App() {
           <Tab.Screen
             name="My"
             options={{
+              header: () => null,
               title: '我',
               tabBarIcon: ({ focused }) => <CoursesSVG width={24} height={24} stroke={focused ? "#0066FF" : "#000000"} />
             }}
---
+++ apps/qmky-app/src/pages/square/index.tsx
@@ -0,0 +1,161 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React, { useEffect, useState } from 'react';
+import { useWindowDimensions, Dimensions, Animated, StyleSheet } from 'react-native'
+import {
+  View,
+  Box,
+  Pressable,
+  Image,
+  Text,
+  Flex,
+  useColorModeValue,
+  Button,
+} from "native-base";
+import { TabView, SceneMap } from 'react-native-tab-view';
+
+// enum ACTIVE_TABS {
+//   TODAY = 'TODAY',
+//   WEEK = 'WEEK',
+//   ALL = 'ALL'
+// }
+
+// const TABS_CONF: {
+//   key: ACTIVE_TABS,
+//   title: string
+// }[] = [{
+//   key: ACTIVE_TABS.TODAY,
+//   title: '今日磕头榜'
+// }, {
+//   key: ACTIVE_TABS.WEEK,
+//   title: '周磕头榜'
+// }, {
+//   key: ACTIVE_TABS.ALL,
+//   title: '历史磕头'
+// }]
+const FirstRoute = () => {
+  return (
+    <Box style={[styles.scene, { backgroundColor: '#ccc', minHeight: 200 }]}>
+      <Flex direction="row" px={16}>
+        <Flex direction="column" pt={16} m={4}>
+          <Image
+            size="lg"
+            source={{
+              uri: 'https://image.flaticon.com/icons/png/512/3079/3079038.png',
+            }}
+            alt="image"
+          />
+        </Flex>
+        <Flex direction="column" pt={16} pr={4}>
+          <Text fontSize={24} py={3}>
+            Skills
+          </Text>
+          <Text pt={4}>
+            Add your skills so that people on the platform can reach you.
+          </Text>
+          <Box py={4} width={32}>
+            <Button size="sm">ADD SKILLS</Button>
+          </Box>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
+
+const SecondRoute = () => (
+  <View style={[styles.scene, { backgroundColor: '#ccc', minHeight: 200 }]}>
+    <Text>12312321312</Text>
+    <Text>12312321312</Text>
+    <Text>12312321312</Text>
+    <Text>12312321312</Text>
+    <Text>12312321312</Text>
+    <Text>12312321312</Text>
+    <Text>12312321312</Text>
+    <Text>12312321312</Text>
+  </View>
+ );
+
+const ThirdRoute = () => (
+  <View style={[styles.scene, { backgroundColor: '#555' }]} />
+ );
+
+const initialLayout = {
+  width: Dimensions.get("window").width,
+};
+const renderScene = SceneMap({
+  first: (...args) => {
+    return <FirstRoute />
+  },
+  second: SecondRoute,
+  third: ThirdRoute,
+});
+
+export function Square({ navigation }) {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([{
+    key: "first",
+    title: "Tab 1"
+  }, {
+    key: "second",
+    title: "Tab 2"
+  }, {
+    key: "third",
+    title: "Tab 3"
+  }]);
+
+  const renderTabBar = props => {
+    const inputRange = props.navigationState.routes.map((x, i) => i);
+    return <Box flexDirection="row">
+        {props.navigationState.routes.map((route, i) => {
+        const opacity = props.position.interpolate({
+          inputRange,
+          outputRange: inputRange.map(inputIndex => inputIndex === i ? 1 : 0.5)
+        });
+        const color = index === i ? "#000" : "#1f2937"
+        const borderColor = index === i ? "cyan.500" : "coolGray.200"
+        return <Box key={route.key} borderBottomWidth="3" borderColor={borderColor} flex={1} alignItems="center" p="3">
+              <Pressable onPress={() => {
+            console.log(i);
+            setIndex(i);
+          }}>
+                <Animated.Text style={{
+              color
+            }}>{route.title}</Animated.Text>
+              </Pressable>
+            </Box>;
+      })}
+      </Box>;
+  };
+
+  return (
+      <TabView
+        navigationState={{
+          index,
+          routes
+        }}
+        renderScene={renderScene}
+        renderTabBar={renderTabBar}
+        onIndexChange={setIndex}
+        initialLayout={initialLayout}
+        lazy
+        sceneContainerStyle={{
+          // marginTop: StatusBar.currentHeight
+          flex: 1,
+          height: 600,
+          borderColor: '#000',
+          borderWidth: 2,
+          // display: 'flex',
+          // justifyContent: 'center',
+          // alignItems: 'center',
+          backgroundColor: '#3322'
+        }}
+      />
+  )
+}
+
+const styles = StyleSheet.create({
+  scene: {
+    flex: 1,
+    height: 600,
+    backgroundColor: '#000'
+  },
+ });
